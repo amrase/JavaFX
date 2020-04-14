4689aaf20cd238e9786b42899a56326d00a2189c
Amra Sezairi <amra.sezairi@hotmail.com>
2020-04-13 22:12:25 -0400

    ColorPicker,DatePicker,Spinner,Accordion

--- GridLayout/src/sample/Main.java
+++ GridLayout/src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("controller.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 500, 275));
+        primaryStage.setScene(new Scene(root, 700, 400));
         primaryStage.show();
     }
 
